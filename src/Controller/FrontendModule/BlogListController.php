87fdceeb1870e7127a741af3588887f4899367bd
Hamid Peywasti <hamid@respinar.com>
2023-08-06 12:47:32 +0330

    Update License notice

--- src/Controller/FrontendModule/BlogListController.php
+++ src/Controller/FrontendModule/BlogListController.php
@@ -3,13 +3,11 @@
 declare(strict_types=1);
 
 /*
- * This file is part of Simple Blog.
+ * This file is part of Simple Blog for Contao.
  *
- * (c) Hamid Abbaszadeh 2023 <abbaszadeh.h@gmail.com>
+ * (c) Hamid Peywasti 2023 <hamid@respinar.com>
+ *
  * @license MIT
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/respinar/contao-simple-blog
  */
 
 namespace Respinar\ContaoSimpleBlog\Controller\FrontendModule;
