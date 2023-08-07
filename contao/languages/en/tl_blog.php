c2ff5781de72940f8decb6931c666fb2c9de84d7
Hamid Peywasti <hamid@respinar.com>
2023-08-07 13:18:33 +0330

    Update License notice

--- contao/languages/en/tl_blog.php
+++ contao/languages/en/tl_blog.php
@@ -3,21 +3,11 @@
 declare(strict_types=1);
 
 /*
-<<<<<<< HEAD
  * This file is part of Simple Blog for Contao.
  *
  * (c) Hamid Peywasti 2023 <hamid@respinar.com>
  *
  * @license MIT
-=======
- * This file is part of Simple Blog.
- *
- * (c) Hamid Abbaszadeh 2023 <abbaszadeh.h@gmail.com>
- * @license MIT
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/respinar/contao-simple-blog
->>>>>>> b047c1b3b92d6b990ae6334b1743d6e2968e8d74
  */
 
 /**
