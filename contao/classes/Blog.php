4e2b1269d7c1655d6d07844ec553671048f5b4b5
Hamid Peywasti <hamid@respinar.com>
2023-08-08 10:30:35 +0330

    Add Blog class

---
+++ contao/classes/Blog.php
@@ -0,0 +1,655 @@
+<?php
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Respinar\SimpleBlogBundle;
+
+use Contao\Frontend;
+use Contao\Input;
+use Contao\System;
+use Contao\Config;
+use Contao\Environment;
+use Contao\StringUtil;
+use Contao\File;
+use contao\Files;
+use Contao\ContentModel;
+use Contao\UserModel;
+use Contao\FilesModel;
+use Contao\PageModel;
+use Contao\ArticleModel;
+use Contao\LayoutModel;
+use Contao\Feed;
+use Contao\FeedItem;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+
+use Respinar\SimpleBlogBundle\Model\BlogArchiveModel;
+use Respinar\SimpleBlogBundle\Model\BlogPostModel;
+use Respinar\SimpleBlogBundle\Model\BlogFeedModel;
+
+/**
+ * Provide methods regarding blog archives.
+ */
+class Blog extends Frontend
+{
+	/**
+	 * URL cache array
+	 * @var array
+	 */
+	private static $arrUrlCache = array();
+
+	/**
+	 * Page cache array
+	 * @var array
+	 */
+	private static $arrPageCache = array();
+
+	/**
+	 * Update a particular RSS feed
+	 *
+	 * @param integer $intId
+	 */
+	public function generateFeed($intId)
+	{
+		$objFeed = BlogFeedModel::findByPk($intId);
+
+		if ($objFeed === null)
+		{
+			return;
+		}
+
+		$objFeed->feedName = $objFeed->alias ?: 'blog' . $objFeed->id;
+
+		// Delete XML file
+		if (Input::get('act') == 'delete')
+		{
+			$this->import(Files::class, 'Files');
+			$this->Files->delete($objFeed->feedName . '.xml');
+		}
+
+		// Update XML file
+		else
+		{
+			$this->generateFiles($objFeed->row());
+
+			System::getContainer()->get('monolog.logger.contao.cron')->info('Generated blog feed "' . $objFeed->feedName . '.xml"');
+		}
+	}
+
+	/**
+	 * Delete old files and generate all feeds
+	 */
+	public function generateFeeds()
+	{
+		$this->import(Automator::class, 'Automator');
+		$this->Automator->purgeXmlFiles();
+
+		$objFeed = BlogFeedModel::findAll();
+
+		if ($objFeed !== null)
+		{
+			while ($objFeed->next())
+			{
+				$objFeed->feedName = $objFeed->alias ?: 'blog' . $objFeed->id;
+				$this->generateFiles($objFeed->row());
+
+				System::getContainer()->get('monolog.logger.contao.cron')->info('Generated blog feed "' . $objFeed->feedName . '.xml"');
+			}
+		}
+	}
+
+	/**
+	 * Generate all feeds including a certain archive
+	 * #
+	 * @param integer $intId
+	 */
+	public function generateFeedsByArchive($intId)
+	{
+		$objFeed = BlogFeedModel::findByArchive($intId);
+
+		if ($objFeed !== null)
+		{
+			while ($objFeed->next())
+			{
+				$objFeed->feedName = $objFeed->alias ?: 'blog' . $objFeed->id;
+
+				// Update the XML file
+				$this->generateFiles($objFeed->row());
+
+				System::getContainer()->get('monolog.logger.contao.cron')->info('Generated blog feed "' . $objFeed->feedName . '.xml"');
+			}
+		}
+	}
+
+	/**
+	 * Generate an XML files and save them to the root directory
+	 *
+	 * @param array $arrFeed
+	 */
+	protected function generateFiles($arrFeed)
+	{
+		$arrArchives = StringUtil::deserialize($arrFeed['archives']);
+
+		if (empty($arrArchives) || !\is_array($arrArchives))
+		{
+			return;
+		}
+
+		$strType = ($arrFeed['format'] == 'atom') ? 'generateAtom' : 'generateRss';
+		$strLink = $arrFeed['feedBase'] ?: Environment::get('base');
+		$strFile = $arrFeed['feedName'];
+
+		$objFeed = new Feed($strFile);
+		$objFeed->link = $strLink;
+		$objFeed->title = $arrFeed['title'];
+		$objFeed->description = $arrFeed['description'];
+		$objFeed->language = $arrFeed['language'];
+		$objFeed->published = $arrFeed['tstamp'];
+
+		// Get the items
+		if ($arrFeed['maxItems'] > 0)
+		{
+			$objArticle = BlogPostModel::findPublishedByPids($arrArchives, null, $arrFeed['maxItems']);
+		}
+		else
+		{
+			$objArticle = BlogPostModel::findPublishedByPids($arrArchives);
+		}
+
+		$container = System::getContainer();
+
+		// Parse the items
+		if ($objArticle !== null)
+		{
+			$arrUrls = array();
+
+			/** @var RequestStack $requestStack */
+			$requestStack = $container->get('request_stack');
+			$currentRequest = $requestStack->getCurrentRequest();
+
+			$time = time();
+			$origObjPage = $GLOBALS['objPage'] ?? null;
+
+			while ($objArticle->next())
+			{
+				// Never add unpublished elements to the RSS feeds
+				if (!$objArticle->published || ($objArticle->start && $objArticle->start > $time) || ($objArticle->stop && $objArticle->stop <= $time))
+				{
+					continue;
+				}
+
+				$jumpTo = $objArticle->getRelated('pid')->jumpTo;
+
+				// No jumpTo page set (see #4784)
+				if (!$jumpTo)
+				{
+					continue;
+				}
+
+				$objParent = $this->getPageWithDetails($jumpTo);
+
+				// A jumpTo page is set but does no longer exist (see #5781)
+				if ($objParent === null)
+				{
+					continue;
+				}
+
+				// Override the global page object (#2946)
+				$GLOBALS['objPage'] = $objParent;
+
+				// Get the jumpTo URL
+				if (!isset($arrUrls[$jumpTo]))
+				{
+					$arrUrls[$jumpTo] = $objParent->getAbsoluteUrl(Config::get('useAutoItem') ? '/%s' : '/items/%s');
+				}
+
+				$strUrl = $arrUrls[$jumpTo];
+
+				$objItem = new FeedItem();
+				$objItem->title = $objArticle->headline;
+				$objItem->link = $this->getLink($objArticle, $strUrl);
+				$objItem->published = $objArticle->date;
+
+				// Push a new request to the request stack (#3856)
+				$request = $this->createSubRequest($objItem->link, $currentRequest);
+				$request->attributes->set('_scope', 'frontend');
+				$requestStack->push($request);
+
+				/** @var UserModel $objAuthor */
+				if (($objAuthor = $objArticle->getRelated('author')) instanceof UserModel)
+				{
+					$objItem->author = $objAuthor->name;
+				}
+
+				// Prepare the description
+				if ($arrFeed['source'] == 'source_text')
+				{
+					$strDescription = '';
+					$objElement = ContentModel::findPublishedByPidAndTable($objArticle->id, 'tl_blog_post');
+
+					if ($objElement !== null)
+					{
+						// Overwrite the request (see #7756)
+						$strRequest = Environment::get('request');
+						Environment::set('request', $objItem->link);
+
+						while ($objElement->next())
+						{
+							$strDescription .= $this->getContentElement($objElement->current());
+						}
+
+						Environment::set('request', $strRequest);
+					}
+				}
+				else
+				{
+					$strDescription = $objArticle->teaser ?? '';
+				}
+
+				$strDescription = $container->get('contao.insert_tag.parser')->replaceInline($strDescription);
+				$objItem->description = $this->convertRelativeUrls($strDescription, $strLink);
+
+				// Add the article image as enclosure
+				if ($objArticle->addImage)
+				{
+					$objFile = FilesModel::findByUuid($objArticle->singleSRC);
+
+					if ($objFile !== null)
+					{
+						$objItem->addEnclosure($objFile->path, $strLink, 'media:content', $arrFeed['imgSize']);
+					}
+				}
+
+				// Enclosures
+				if ($objArticle->addEnclosure)
+				{
+					$arrEnclosure = StringUtil::deserialize($objArticle->enclosure, true);
+
+					if (\is_array($arrEnclosure))
+					{
+						$objFile = FilesModel::findMultipleByUuids($arrEnclosure);
+
+						if ($objFile !== null)
+						{
+							while ($objFile->next())
+							{
+								$objItem->addEnclosure($objFile->path, $strLink);
+							}
+						}
+					}
+				}
+
+				$objFeed->addItem($objItem);
+
+				$requestStack->pop();
+			}
+
+			$GLOBALS['objPage'] = $origObjPage;
+		}
+
+		$webDir = StringUtil::stripRootDir($container->getParameter('contao.web_dir'));
+
+		// Create the file
+		File::putContent($webDir . '/share/' . $strFile . '.xml', $container->get('contao.insert_tag.parser')->replaceInline($objFeed->$strType()));
+	}
+
+	/**
+	 * Add blog items to the indexer
+	 *
+	 * @param array   $arrPages
+	 * @param integer $intRoot
+	 * @param boolean $blnIsSitemap
+	 *
+	 * @return array
+	 */
+	public function getSearchablePages($arrPages, $intRoot=0, $blnIsSitemap=false)
+	{
+		$arrRoot = array();
+
+		if ($intRoot > 0)
+		{
+			$arrRoot = $this->Database->getChildRecords($intRoot, 'tl_page');
+		}
+
+		$arrProcessed = array();
+		$time = time();
+
+		// Get all blog archives
+		$objArchive = BlogArchiveModel::findByProtected('');
+
+		// Walk through each archive
+		if ($objArchive !== null)
+		{
+			while ($objArchive->next())
+			{
+				// Skip blog archives without target page
+				if (!$objArchive->jumpTo)
+				{
+					continue;
+				}
+
+				// Skip blog archives outside the root nodes
+				if (!empty($arrRoot) && !\in_array($objArchive->jumpTo, $arrRoot))
+				{
+					continue;
+				}
+
+				// Get the URL of the jumpTo page
+				if (!isset($arrProcessed[$objArchive->jumpTo]))
+				{
+					$objParent = PageModel::findWithDetails($objArchive->jumpTo);
+
+					// The target page does not exist
+					if ($objParent === null)
+					{
+						continue;
+					}
+
+					// The target page has not been published (see #5520)
+					if (!$objParent->published || ($objParent->start && $objParent->start > $time) || ($objParent->stop && $objParent->stop <= $time))
+					{
+						continue;
+					}
+
+					if ($blnIsSitemap)
+					{
+						// The target page is protected (see #8416)
+						if ($objParent->protected)
+						{
+							continue;
+						}
+
+						// The target page is exempt from the sitemap (see #6418)
+						if ($objParent->robots == 'noindex,nofollow')
+						{
+							continue;
+						}
+					}
+
+					// Generate the URL
+					$arrProcessed[$objArchive->jumpTo] = $objParent->getAbsoluteUrl(Config::get('useAutoItem') ? '/%s' : '/items/%s');
+				}
+
+				$strUrl = $arrProcessed[$objArchive->jumpTo];
+
+				// Get the items
+				$objArticle = BlogPostModel::findPublishedDefaultByPid($objArchive->id);
+
+				if ($objArticle !== null)
+				{
+					while ($objArticle->next())
+					{
+						if ($blnIsSitemap && $objArticle->robots === 'noindex,nofollow')
+						{
+							continue;
+						}
+
+						$arrPages[] = $this->getLink($objArticle, $strUrl);
+					}
+				}
+			}
+		}
+
+		return $arrPages;
+	}
+
+	/**
+	 * Generate a URL and return it as string
+	 *
+	 * @param BlogPostModel $objItem
+	 * @param boolean   $blnAddArchive
+	 * @param boolean   $blnAbsolute
+	 *
+	 * @return string
+	 */
+	public static function generateBlogUrl($objItem, $blnAddArchive=false, $blnAbsolute=false)
+	{
+		$strCacheKey = 'id_' . $objItem->id . ($blnAbsolute ? '_absolute' : '');
+
+		// Load the URL from cache
+		if (isset(self::$arrUrlCache[$strCacheKey]))
+		{
+			return self::$arrUrlCache[$strCacheKey];
+		}
+
+		// Initialize the cache
+		self::$arrUrlCache[$strCacheKey] = null;
+
+		switch ($objItem->source)
+		{
+			// Link to an external page
+			case 'external':
+				if (0 === strncmp($objItem->url, 'mailto:', 7))
+				{
+					self::$arrUrlCache[$strCacheKey] = StringUtil::encodeEmail($objItem->url);
+				}
+				else
+				{
+					self::$arrUrlCache[$strCacheKey] = StringUtil::ampersand($objItem->url);
+				}
+				break;
+
+			// Link to an internal page
+			case 'internal':
+				if (($objTarget = $objItem->getRelated('jumpTo')) instanceof PageModel)
+				{
+					/** @var PageModel $objTarget */
+					self::$arrUrlCache[$strCacheKey] = StringUtil::ampersand($blnAbsolute ? $objTarget->getAbsoluteUrl() : $objTarget->getFrontendUrl());
+				}
+				break;
+
+			// Link to an article
+			case 'article':
+				if (($objArticle = ArticleModel::findByPk($objItem->articleId)) instanceof ArticleModel && ($objPid = $objArticle->getRelated('pid')) instanceof PageModel)
+				{
+					$params = '/articles/' . ($objArticle->alias ?: $objArticle->id);
+
+					/** @var PageModel $objPid */
+					self::$arrUrlCache[$strCacheKey] = StringUtil::ampersand($blnAbsolute ? $objPid->getAbsoluteUrl($params) : $objPid->getFrontendUrl($params));
+				}
+				break;
+		}
+
+		// Link to the default page
+		if (self::$arrUrlCache[$strCacheKey] === null)
+		{
+			$objPage = PageModel::findByPk($objItem->getRelated('pid')->jumpTo);
+
+			if (!$objPage instanceof PageModel)
+			{
+				self::$arrUrlCache[$strCacheKey] = StringUtil::ampersand(Environment::get('request'));
+			}
+			else
+			{
+				$params = (Config::get('useAutoItem') ? '/' : '/items/') . ($objItem->alias ?: $objItem->id);
+
+				self::$arrUrlCache[$strCacheKey] = StringUtil::ampersand($blnAbsolute ? $objPage->getAbsoluteUrl($params) : $objPage->getFrontendUrl($params));
+			}
+
+			// Add the current archive parameter (blog archive)
+			if ($blnAddArchive && Input::get('month'))
+			{
+				self::$arrUrlCache[$strCacheKey] .= '?month=' . Input::get('month');
+			}
+		}
+
+		return self::$arrUrlCache[$strCacheKey];
+	}
+
+	/**
+	 * Return the schema.org data from a blog article
+	 *
+	 * @param BlogPostModel $objArticle
+	 *
+	 * @return array
+	 */
+	public static function getSchemaOrgData(BlogPostModel $objArticle): array
+	{
+		$htmlDecoder = System::getContainer()->get('contao.string.html_decoder');
+
+		$jsonLd = array(
+			'@type' => 'BlogArticle',
+			'identifier' => '#/schema/blog/' . $objArticle->id,
+			'url' => self::generateBlogUrl($objArticle),
+			'headline' => $htmlDecoder->inputEncodedToPlainText($objArticle->headline),
+			'datePublished' => date('Y-m-d\TH:i:sP', $objArticle->date),
+		);
+
+		if ($objArticle->teaser)
+		{
+			$jsonLd['description'] = $htmlDecoder->htmlToPlainText($objArticle->teaser);
+		}
+
+		/** @var UserModel $objAuthor */
+		if (($objAuthor = $objArticle->getRelated('author')) instanceof UserModel)
+		{
+			$jsonLd['author'] = array(
+				'@type' => 'Person',
+				'name' => $objAuthor->name,
+			);
+		}
+
+		return $jsonLd;
+	}
+
+	/**
+	 * Return the link of a blog article
+	 *
+	 * @param BlogPostModel $objItem
+	 * @param string    $strUrl
+	 * @param string    $strBase
+	 *
+	 * @return string
+	 */
+	protected function getLink($objItem, $strUrl, $strBase='')
+	{
+		switch ($objItem->source)
+		{
+			// Link to an external page
+			case 'external':
+				return $objItem->url;
+
+			// Link to an internal page
+			case 'internal':
+				if (($objTarget = $objItem->getRelated('jumpTo')) instanceof PageModel)
+				{
+					/** @var PageModel $objTarget */
+					return $objTarget->getAbsoluteUrl();
+				}
+				break;
+
+			// Link to an article
+			case 'article':
+				if (($objArticle = ArticleModel::findByPk($objItem->articleId)) instanceof ArticleModel && ($objPid = $objArticle->getRelated('pid')) instanceof PageModel)
+				{
+					/** @var PageModel $objPid */
+					return StringUtil::ampersand($objPid->getAbsoluteUrl('/articles/' . ($objArticle->alias ?: $objArticle->id)));
+				}
+				break;
+		}
+
+		// Backwards compatibility (see #8329)
+		if ($strBase && !preg_match('#^https?://#', $strUrl))
+		{
+			$strUrl = $strBase . $strUrl;
+		}
+
+		// Link to the default page
+		return sprintf(preg_replace('/%(?!s)/', '%%', $strUrl), ($objItem->alias ?: $objItem->id));
+	}
+
+	/**
+	 * Return the names of the existing feeds so they are not removed
+	 *
+	 * @return array
+	 */
+	public function purgeOldFeeds()
+	{
+		$arrFeeds = array();
+		$objFeeds = BlogFeedModel::findAll();
+
+		if ($objFeeds !== null)
+		{
+			while ($objFeeds->next())
+			{
+				$arrFeeds[] = $objFeeds->alias ?: 'blog' . $objFeeds->id;
+			}
+		}
+
+		return $arrFeeds;
+	}
+
+	/**
+	 * Return the page object with loaded details for the given page ID
+	 *
+	 * @param  integer        $intPageId
+	 * @return PageModel|null
+	 */
+	private function getPageWithDetails($intPageId)
+	{
+		if (!\array_key_exists($intPageId, self::$arrPageCache))
+		{
+			$objPage = self::$arrPageCache[$intPageId] = PageModel::findWithDetails($intPageId);
+
+			if (null === $objPage)
+			{
+				return null;
+			}
+
+			$objLayout = $objPage->getRelated('layout');
+
+			if (!$objLayout instanceof LayoutModel)
+			{
+				return $objPage;
+			}
+
+			/** @var ThemeModel $objTheme */
+			$objTheme = $objLayout->getRelated('pid');
+			$objPage->templateGroup = $objTheme->templates ?? null;
+		}
+
+		return self::$arrPageCache[$intPageId];
+	}
+
+	/**
+	 * Creates a sub request for the given URI.
+	 */
+	private function createSubRequest(string $uri, Request $request = null): Request
+	{
+		$cookies = null !== $request ? $request->cookies->all() : array();
+		$server = null !== $request ? $request->server->all() : array();
+
+		unset($server['HTTP_IF_MODIFIED_SINCE'], $server['HTTP_IF_NONE_MATCH']);
+
+		$subRequest = Request::create($uri, 'get', array(), $cookies, array(), $server);
+
+		if (null !== $request)
+		{
+			if ($request->get('_format'))
+			{
+				$subRequest->attributes->set('_format', $request->get('_format'));
+			}
+
+			if ($request->getDefaultLocale() !== $request->getLocale())
+			{
+				$subRequest->setLocale($request->getLocale());
+			}
+		}
+
+		// Always set a session (#3856)
+		$subRequest->setSession(new Session(new MockArraySessionStorage()));
+
+		return $subRequest;
+	}
+}
+
+class_alias(Blog::class, 'Blog');
